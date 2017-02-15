8d2b20d331a3ba45bc61a34432f2d0901cc2c416
Linh Tran <linhtran17495@gmail.com>
2017-02-16 01:09:44 +0700

    change port for heroku

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.get('/register/:name', (req, res)=>{
 })
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
