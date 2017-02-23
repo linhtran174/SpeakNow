80fe4150ba5afb220fc40b68470191ce0a526aa0
Linh Tran <linhtran17495@gmail.com>
2017-02-23 17:28:48 +0700

    Register user ok

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
 var fs = require('fs'),
 	uws = require('uws');
-
 var wss = new uws.Server({nativeHttp: true, port: 3000}),
 	http = require('http');
 
 server = http.createServer((req, res) => {
 	res.end(fs.readFileSync('index.html'));
-});
-server.listen(80);
+}).listen(80);
+//////////////////END INIT SERVER//////////////////////////
 
-wss.on('connection', function(ws) {
-    console.log('ok, connection');
+
+var exampleUser = 
+{'Linh Tran' : {peerId: '1io340a4i6hh8b3c'}};
+var users = [];
+
+var processMessage = function(s, m){
+	m = JSON.parse(m);
+	if(m.newUser){
+		if(users[m.user.name]){
+			s.send(JSON.stringify({
+				userExisted: true,
+				name: m.user.name
+			}))
+		}
+		else{
+			users[m.user.name] = m.user.peerId;
+			console.log("registerSuccess");
+			s.send(JSON.stringify({
+				registerSuccess: true,
+				name: m.user.name
+			}));
+			wss.clients.forEach(
+			(peer)=>{
+				if(peer.readyState !== uws.OPEN) return;
+				console.log("addUser");
+				peer.send(JSON.stringify({
+					addUser: true,
+					user: m.user
+				}))
+			});
+		}
+		return;
+	}
+	if(m.getUserLists){
+		user.send(JSON.stringify({
+			userList : users
+		}));
+	}
+
+}
+
+
+
+wss.on('connection', function(socket) {
+	socket.on('message', (message)=>{
+		processMessage(socket, message);
+	});
+});
+
+wss.on('message', ()=>{
+	console.log('message received!');
 });
 
 wss.on('error', function(error) {
