faf7c5607ac8c605f270c9d9e6113eae7057bf81
Linh Tran <linhtran17495@gmail.com>
2017-03-13 17:27:28 +0700

    refactor code

--- index.js
+++ index.js
@@ -1,59 +1,9 @@
 var fs = require('fs'),
 	uws = require('uws'),
-	http = require('http'),
-	https = require('https');
+	http = require('lib/initServer');
 
-
-var sslCerl = {
-	key: fs.readFileSync('SSL_cert/domain-key.txt'),
-	cert: fs.readFileSync('SSL_cert/domain-crt.txt')
-}
-
-var server = https.createServer(sslCerl, (req,res)=>{
-	res.end(fs.readFileSync('index.html'));
-}).listen(443);
-
-var wss = new uws.Server({server: server, port: 3000});
-
-//return AMCE_reponse{ "text": <responseText>} if the request is a ACME challenge
-var AMCE_Challenge = function(req){
-	if(req.url == "/.well-known/acme-challenge/XD0ccAIWW4MHFr7cBQ_pTD7Y1r0dDSzT9Wiy36m2Ejw")
-		return { text:
-		 "XD0ccAIWW4MHFr7cBQ_pTD7Y1r0dDSzT9Wiy36m2Ejw.wTz1G0JCJRVKSFedh2VPiCGquf2YwBfr0Ega2C0FQH0"};
-	if(req.url == "/.well-known/acme-challenge/QFyek2FGSTU3BKDxNQ7MglBQ4auN9bS9idejPffpmdw")
-		return { text: 
-		 "QFyek2FGSTU3BKDxNQ7MglBQ4auN9bS9idejPffpmdw.wTz1G0JCJRVKSFedh2VPiCGquf2YwBfr0Ega2C0FQH0"};
-	return null;
-}
-
-http.createServer((req, res) => {
-	console.log(req.url);
-	var challenge = AMCE_Challenge(req);
-	if(challenge){
-		res.end(challenge.text);
-	}
-	else{
-		//redirect to HTTPS
-		res.writeHead(301,{
-			'Location' : 'https://xseed.tech'
-		});
-		res.end();
-	}
-
-}).listen(80);
-
+var wss = new uws.Server({server: http, port: 3000});
 //////////////////END INIT SERVER//////////////////////////
-
-
-//library
-function strMapToObj(strMap) {
-	let obj = Object.create(null);
-    for (let [k,v] of strMap) {
-        obj[k] = v;
-    }
-    return obj;
-}
-
 var usersMap = new Map();
 
 var processMessage = function(s, m){
@@ -97,6 +47,7 @@ var processMessage = function(s, m){
 }
 
 wss.on('disconnection', (me)=>{
+	console.log('disconnection');
 	wss.clients.forEach((client)=>{
 		if(client === me || client.readyState !== uws.OPEN) return;
 		console.log('delUser');
@@ -120,4 +71,3 @@ wss.on('message', ()=>{
 wss.on('error', function(error) {
     console.log('Cannot start server');
 });
-
