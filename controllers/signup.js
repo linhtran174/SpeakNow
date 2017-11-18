54cbb5270646b5e669ed3259f300d5a302f59d33
Linh Tran <linhtran17495@gmail.com>
2017-11-18 17:59:06 +0700

    sign up api

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,3 +1,24 @@
-module.exports = function(req, res){
+var bodyParser = require('body-parser').json();
+var md5 = require('crypto').createHash('md5');
 
+module.exports = function(req, res, libs){
+	var db = libs.db;
+	console.log(req.headers)
+	email = req.headers.email;
+	password = md5.update(req.headers.password).digest("hex");
+
+	db.query("SELECT COUNT(email) FROM users where email = ?", [email],
+	(err, docs, fields) => {
+		if(docs.length != 0){
+			res.end(JSON.stringify({message : "Email has already been registed"}));
+		}
+
+		db.query("INSERT INTO users VALUES (?,?)", [email, password], (err, res, libs) => {
+			if(err) res.end(JSON.stringify({message : error}))
+
+			res.end(JSON.stringify(
+				message: "sign up success"
+			))
+		})
+	})
 }
--- testing.js
+++ testing.js
@@ -1,31 +1,4 @@
-function strMapToJSON(strMap) {
-	let obj = Object.create(null);
-    for (let [k,v] of strMap) {
-        obj[k] = v;
-    }
-    return JSON.stringify(obj);
-}
-
-var usersMap = new Map();
-
-usersMap.set('1', 'sdasdasdasd');
-usersMap.set('2', 'sdasdasdasdasda');
-
-usersMap.forEach((value, key)=>{
-	console.log(key + " " + value);
-})
-
-
-console.log(usersMap.get('2'));
-
-
-function objToStrMap(obj) {
-	let strMap = new Map();
-	for (let k of Object.keys(obj)) {
-	    strMap.set(k, obj[k]);
-	}
-	return strMap;
-}
-
-
-console.log(objToStrMap(JSON.parse(strMapToJSON(usersMap))));
+require('http').createServer((req, res) => {
+	console.log(req)
+	
+}).listen(80);
