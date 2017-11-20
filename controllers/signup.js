7f056df882c9e97e5a6930ce30a9f71f1e55ca1a
root <linhtran17495@gmail.com>
2017-11-20 22:43:36 +0700

    signUp api tested

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,24 +1,35 @@
-var bodyParser = require('body-parser').json();
-var md5 = require('crypto').createHash('md5');
+var crypto = require('crypto');
+
+var md5 = (string)=>{
+	return crypto.createHash('md5').update(string).digest("hex");
+}
 
 module.exports = function(req, res, libs){
 	var db = libs.db;
 	console.log(req.headers)
 	email = req.headers.email;
-	password = md5.update(req.headers.password).digest("hex");
+	password = md5(req.headers.password);
 
-	db.query("SELECT COUNT(email) FROM users where email = ?", [email],
+	db.query("SELECT * FROM users where email = ?", [email],
 	(err, docs, fields) => {
 		if(docs.length != 0){
-			res.end(JSON.stringify({message : "Email has already been registed"}));
+			res.end(JSON.stringify({
+				message : "Email has already been registed",
+				status: "failed",
+			}));
+			return;
 		}
 
-		db.query("INSERT INTO users VALUES (?,?)", [email, password], (err, res, libs) => {
-			if(err) res.end(JSON.stringify({message : error}))
+		db.query("INSERT INTO users VALUES (?,?)", [email, password], (err, docs, fields) => {
+			if(err){
+				res.end(JSON.stringify({message : error, status: "failed"}))
+				return;	
+			}
 
-			res.end(JSON.stringify(
-				message: "sign up success"
-			))
+			res.end(JSON.stringify({
+				message: "sign up success",
+				status : "success"
+			}));
 		})
 	})
 }
