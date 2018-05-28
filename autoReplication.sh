86ad8e19ef83d4f658e9a692a0b4a0b2a6139384
Linh Tran <linhtran17495@gmail.com>
2018-05-28 09:29:05 +0000

    fix wrong port in autoReplication script

--- autoReplication.sh
+++ autoReplication.sh
@@ -7,10 +7,10 @@ sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 3001 -m statistic --mod
 sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 3001 -m statistic --mode nth --every 2 --packet 0 -j REDIRECT --to-ports 3004
 sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 3001 -j REDIRECT --to-ports 3005
 
-sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9000 -m statistic --mode nth --every 4 --packet 0 -j REDIRECT --to-ports 9001
-sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9000 -m statistic --mode nth --every 3 --packet 0 -j REDIRECT --to-ports 3002
-sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9000 -m statistic --mode nth --every 2 --packet 0 -j REDIRECT --to-ports 3003
-sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9000 -j REDIRECT --to-ports 3004
+sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9000 -m statistic --mode nth --every 4 --packet 0 -j REDIRECT --to-ports 9002
+sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9000 -m statistic --mode nth --every 3 --packet 0 -j REDIRECT --to-ports 9003
+sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9000 -m statistic --mode nth --every 2 --packet 0 -j REDIRECT --to-ports 9004
+sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport 9000 -j REDIRECT --to-ports 9005
 
 nohup node index.js 3002 9002 &
 nohup node index.js 3003 9003 &
